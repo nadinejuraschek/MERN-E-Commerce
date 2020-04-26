da4f16a3576f6c958fbb728b0b6f56f6b6e469e8
Nadine Ju <nadine.jura@online.de>
2020-04-26 16:49:47 -0700

    product validation added

--- controllers/product.js
+++ controllers/product.js
@@ -14,9 +14,22 @@ exports.create = (req, res) => {
             });
         };
 
+        const { name, description, price, category, quantity, shipping} = fields;
+
+        if (!name || !description || !price || !category || !quantity || !shipping) {
+            return res.status(400).json({
+                error: 'All fields are required.'
+            });
+        };
+
         let product = new Product(fields);
 
         if (files.photo) {
+            if (files.photo.size > 1000000) {
+                return res.status(400).json({
+                    error: 'Image should be less than 1MB in size.'
+                });
+            };
             product.photo.data = fs.readFileSync(files.photo.path);
             product.photo.contentType = files.photo.type;
         };
